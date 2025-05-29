aacdac4d8c5082c60229a5e7763d9c17c062b252
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-05-29 10:10:21 -0300

    add botao na pagina infomracao

--- scripts/login.js
+++ scripts/login.js
@@ -35,3 +35,21 @@ if (!OTP || !OTP.lenght == 5 || isNaN(OTP)) {
 function redirecionar() {
     window.location.href = "paginaGastos2.html";
 }
+function redirecionar2() {
+    window.location.href = "paginaMenuPrincipal.html";
+}
+function redirecionar3() {
+    window.location.href = "paginaSelecioneLinhas.html";
+}
+function redirecionar4() {
+    window.location.href = "paginaSelecioneLinhas.html";
+}
+function redirecionar5() {
+    window.location.href = "paginaSelecioneLinhas.html";
+}
+function redirecionar6() {
+    window.location.href = "paginaSelecioneLinhas.html";
+}
+function redirecionar7() {
+    window.location.href = "paginaSelecioneLinhas.html";
+}
