9d0d1c5286268bf1f5fa6b06f6deed999c3eed2b
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-05-30 08:17:15 -0300

    add hiper links

--- scripts/login.js
+++ scripts/login.js
@@ -42,14 +42,17 @@ function redirecionar3() {
     window.location.href = "paginaSelecioneLinhas.html";
 }
 function redirecionar4() {
-    window.location.href = "paginaSelecioneLinhas.html";
+    window.location.href = "paginaTrensAtivados1.html";
 }
 function redirecionar5() {
-    window.location.href = "paginaSelecioneLinhas.html";
+    window.location.href = "paginaControledeInspeção.html";
 }
 function redirecionar6() {
-    window.location.href = "paginaSelecioneLinhas.html";
+    window.location.href = "paginaRelatorioeAnalises.html";
 }
 function redirecionar7() {
-    window.location.href = "paginaSelecioneLinhas.html";
+    window.location.href = "paginaOuvidoriaGeral.html";
+}
+function redirecionar8() {
+    window.location.href = "paginaAlertaseNotificacoes1.html";
 }
