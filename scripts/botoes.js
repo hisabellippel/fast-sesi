e5e711b8f247cd1ef01af53d5c2c30b349e9a756
Ana <ana.l.amaral@estudante.sesisenai.org.br>
2025-09-19 08:02:27 -0300

    Add - botao

--- scripts/botoes.js
+++ scripts/botoes.js
@@ -1,40 +1,40 @@
 function redirecionar() {
-    window.location.href = "paginaGastos2.html";
+    window.location.href = "paginaGastos2.php";
 }
 function redirecionar2() {
-    window.location.href = "paginaMenuPrincipal.html";
+    window.location.href = "paginaMenuPrincipal.php";
 }
 function redirecionar3() {
-    window.location.href = "paginaSelecioneLinhas.html";
+    window.location.href = "paginaSelecioneLinhas.php";
 }
 function redirecionar4() {
-    window.location.href = "paginaTrensAtivados1.html";
+    window.location.href = "paginaTrensAtivados1.php";
 }
 function redirecionar5() {
-    window.location.href = "paginaControledeInspeção.html";
+    window.location.href = "paginaControledeInspeção.php";
 }
 function redirecionar6() {
-    window.location.href = "paginaRelatorioeAnalises.html";
+    window.location.href = "paginaRelatorioeAnalises.php";
 }
 function redirecionar7() {
-    window.location.href = "paginaOuvidoriaGeral.html";
+    window.location.href = "paginaOuvidoriaGeral.php";
 }
 function redirecionar8() {
-    window.location.href = "paginaAlertaseNotificacoes1.html";
+    window.location.href = "paginaAlertaseNotificacoes1.php";
 }
 
 
 
 
 function redirecionar11() {
-    window.location.href = "paginaGastos3.html";
+    window.location.href = "paginaGastos3.php";
 }
 function redirecionar12() {
-    window.location.href = "paginaGastos4.html";
+    window.location.href = "paginaGastos4.php";
 }
 function redirecionar13() {
-    window.location.href = "paginaGastos5.html";
+    window.location.href = "paginaGastos5.php";
 }
 function redirecionar14() {
-    window.location.href = "paginaGastos6.html";
+    window.location.href = "paginaGastos6.php";
 }
