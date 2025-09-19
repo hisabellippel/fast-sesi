cdfca7d5b31416371c7834421a680edd2e36c081
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-09-19 08:06:22 -0300

    add - style selecione linhas

--- public/paginaSelecioneLinhas.php
+++ public/paginaSelecioneLinhas.php
@@ -12,71 +12,68 @@
             <img class="topo2" src="../asets/imagens/barraAcima/tradutor.png" alt="">
         </div>
     </header>
+    
     <br>
-<main>
-    <div id="azul">
-        <h2> Linhas:</h2>
-    </div>
+
+    <main>
+        <div id="azul">
+            <h2>Linhas:</h2>
+        </div>
+
         <div class="quadradomaior2">
-            
-        <div class="linhaso">
-            <div class="umdo">
-            <h3>Selecione a Linha:</h3> 
-            <img src="../asets/imagens/meio/setaabaixar.png" alt="">
-            <div class="barrapequena"><div class="barrap"></div></div>
+            <div class="linhaso">
+                <div class="umdo">
+                    <h3>Selecione a Linha:</h3> 
+                    <img src="../asets/imagens/meio/setaabaixar.png" alt="">
+                    <div class="barrapequena">
+                        <div class="barrap"></div>
+                    </div>
+                </div>
+
+                <div class="quadrados">       
+                    <a href="paginaSelecioneLinhas1.php">
+                        <div class="quadradoslinhas">
+                            <p>Linha001</p>
+                        </div>
+                    </a>
+
+                    <a href="paginaSelecioneLinhas2.php">
+                        <div class="quadradoslinhas">
+                            <p>Linha002</p>   
+                        </div>
+                    </a>
+
+                    <a href="paginaSelecioneLinhas3.php">
+                        <div class="quadradoslinhas">
+                            <p>Linha003</p>
+                        </div>    
+                    </a>
+
+                    <a href="paginaSelecioneLinhas4.php">
+                        <div class="quadradoslinhas">
+                            <p>Linha004</p>
+                        </div>   
+                    </a>
+
+                    <a href="paginaSelecionelinhas5.php">
+                        <div class="quadradoslinhas">
+                            <p>Linha005</p>
+                        </div>   
+                    </a>
+                </div>
+            </div>
         </div>
-    <div class="quadrados">       
+    </main>
 
-        <a href="paginaSelecioneLinhas1.php">
-        <div class="quadradoslinhas">
-            <p>Linha001</p>
-        </div>
-        </a>
-
-        <a href="paginaSelecioneLinhas2.php">
-        <div class="quadradoslinhas">
-            <p>Linha002</p>   
-        </div>
-        </a>
-
-        <a href="paginaSelecioneLinhas3.php">
-        <div class="quadradoslinhas">
-            <p>Linha003</p>
-        </div>    
-        </a>
-        
-        <a href="paginaSelecioneLinhas4.php">
-        <div class="quadradoslinhas">
-            <p>Linha004</p>
-        </div>   
-        </a>
-
-        <a href="paginaSelecionelinhas5.php">
-        <div class="quadradoslinhas">
-            <p>Linha005</p>
-        </div>   
-        </a>
-    </div>
-    
-    
-    </div>
-
-
-</div>
-    
-
-</main>
-<footer> 
-    <div id="barra777">
+    <footer> 
+        <div id="barra777">
             <a href="paginainformacoes.php"><img class="topo1" src="../asets/imagens/barraAbaixo/barras.png" alt=""></a>
             <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
             <h3>Fast.sesi</h3>
             <a href="paginaNotificacoes.php"><img class="im2" src="../asets/imagens/barraAbaixo/sinoNotificacao.png" alt=""></a>
             <a href="paginaAlterarPerfil.php"><img class="im3" src="../asets/imagens/meio/perfil.png" alt=""></a>
             <a href="paginaPesquisar.php"><img class="im4" src="../asets/imagens/barraAbaixo/Lupa1.png" alt=""></a>
-    </div>
-</footer>
-
-
+        </div>
+    </footer>
 </body>
 </html>
