fdf2a10a489b3ff8aec631df279eb29bb16242f2
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-09-19 08:56:55 -0300

    add arrumei linhas 4 e 5

---
+++ public/paginaSelecioneLinhas4.php
@@ -0,0 +1,65 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selecione Linhas</title>
+    <link rel="stylesheet" href="../style/styles.css">
+</head>
+<body>
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selecione Linhas</title>
+    <link rel="stylesheet" href="../style/styles.css">
+</head>
+<body>
+   <header>
+        <div id="barraescura">
+            <a href="paginaSelecioneLinhas.php"><img class="topo1" src="../asets/imagens/barraAcima/flecha.png" alt=""></a>
+            <img class="topo2" src="../asets/imagens/barraAcima/tradutor.png" alt="">
+        </div>
+    </header>
+<br>
+    <main>
+        <div id="azul">
+            <h2> Linhas:</h2>
+        </div>
+
+       
+        <div class="quadradomaior">
+            <h3>Linha003:</h3> 
+
+            <img class="imglinha" src="../asets/imagens/meio/mapa.png" alt="Mapa da linha">
+
+            <div class="linhaa">
+                <h3>Velocidade: 60 km/h</h3>
+            </div>
+
+            <div class="linhaa">
+                <h3>Passageiros em tempo real: 210</h3>
+            </div>
+
+            <div class="linhaa">
+                <h3>Avisos: Partindo da estação inicial.</h3>
+            </div>
+        </div>
+  
+        <div class="barrapequena2"></div>
+    </main>
+
+    <footer> 
+        <div id="barra778">
+            <a href="paginainformacoes.php"><img class="topo1" src="../asets/imagens/barraAbaixo/barras.png" alt=""></a>
+            <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
+            <h3>Fast.sesi</h3>
+            <a href="paginaNotificacoes.php"><img class="im2" src="../asets/imagens/barraAbaixo/sinoNotificacao.png" alt=""></a>
+            <a href="paginaAlterarPerfil.php"><img class="im3" src="../asets/imagens/meio/perfil.png" alt=""></a>
+            <a href="paginaPesquisar.php"><img class="im4" src="../asets/imagens/barraAbaixo/Lupa1.png" alt=""></a>
+        </div>
+    </footer>
+</body>
+</html>
+
+</body>
+</html>
