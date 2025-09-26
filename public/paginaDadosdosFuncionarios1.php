cf55cdb8d08ec260a357d55c3dbbd330b254ea49
Ana <ana.l.amaral@estudante.sesisenai.org.br>
2025-09-26 08:11:54 -0300

    Add - dados dos funcionarios

--- public/paginaDadosdosFuncionarios1.php
+++ public/paginaDadosdosFuncionarios1.php
@@ -14,7 +14,6 @@
             <img class="topo2" src="../asets/imagens/barraAcima/tradutor.png" alt="">
         </div>
     </header>
-    <br>
 <main>
     <div class="redondaofc">
         <strong><p>Dados dos Funcionários</p></strong> 
