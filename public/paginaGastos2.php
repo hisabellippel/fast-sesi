04cce165615620d0dc967608775e4512bf6753dd
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-10-03 08:23:17 -0300

    add- novas divs

--- public/paginaGastos2.php
+++ public/paginaGastos2.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gastos</title>
+    <link rel="stylesheet" href="../style/style2.css">
     <link rel="stylesheet" href="../style/styles.css">
     <source src="login.js" type="">
 </head>
@@ -17,42 +18,37 @@
         </div>
     </header>
     <br>
-    
+
     <main>
-     
-
-        <div class="redonda3">
-            <p>Gastos <p class="red"></p>
-        </div>
-        <div class="informacao23">
-            <br>
-            <div class="gastos2">
-                <br>
-                <p class="gastos"> ▼Funcionários</p>
-                <h6 class="gastos3">Salário Operacionais: R$ 2.200,00 p/pessoa</h6>
-                <h6 class="gastos3">Salário da Limpeza: R$ 1.600,00 p/pessoa</h6>
-                <h6 class="gastos3">Salário dos Maquinistas:R$ 1.600,00 p/pessoa.</h6>
-                <h6 class="gastos3">Salário dos Técnicos de Manutenção: R$ 2.300,00 p/pessoa</h6>
-                <h6 class="gastos3">Salário dos Engenheiros ferroviários: R$ 3.600,00 p/pessoa</h6>
-                <h6 class="gastos3">Controlador de Tráfego: R$ 1.518,00 p/pessoa</h6>
-                
+        <div class="main-container">
+            <div class="redonda3">
+                <p>Gastos <p class="red"></p>
             </div>
-            
-          
-            
+            <div class="container-gastos">
+                <div class="gastos2">
+                    <br>
+                    <p class="gastos"> ▼Funcionários</p>
+                    <h6 class="gastos3">Salário Operacionais: R$ 2.200,00 p/pessoa</h6>
+                    <h6 class="gastos3">Salário da Limpeza: R$ 1.600,00 p/pessoa</h6>
+                    <h6 class="gastos3">Salário dos Maquinistas:R$ 1.600,00 p/pessoa.</h6>
+                    <h6 class="gastos3">Salário dos Técnicos de Manutenção: R$ 2.300,00 p/pessoa</h6>
+                    <h6 class="gastos3">Salário dos Engenheiros ferroviários: R$ 3.600,00 p/pessoa</h6>
+                    <h6 class="gastos3">Controlador de Tráfego: R$ 1.518,00 p/pessoa</h6>
+                </div>
+            </div>
+
+            <div class="redonda3">
+                <p>Gráficos</p>
+            </div>
+            <div class="container-graficos">
+                <div class="informacao2">
+                    <br>
+                    <img class="grafico2 grafico2-large" src="../asets/imagens/meio/grafico1.png" alt="">
+                </div>
+            </div>
         </div>
 
-        <div class="redonda3">
-            <p>Gráficos</p>
-        </div>
-        <div class="informacao2">
-            <br>
-            <img class="grafico2" src="../asets/imagens/meio/grafico1.png" alt="">
-            
-            
-        </div>
-
-      <br><br>
+        <br><br>
 
         <div id="barra">
             <a href="paginainformacoes.php"><img class="topo1" src="../asets/imagens/barraAbaixo/barras.png" alt=""></a>
@@ -62,7 +58,7 @@
             <a href="paginaAlterarPerfil.php"><img class="im3" src="../asets/imagens/meio/perfil.png" alt=""></a>
             <a href="paginaPesquisar.php"><img class="im4" src="../asets/imagens/barraAbaixo/Lupa1.png" alt=""></a>
         </div>
-       
+
     </main>
 
 
