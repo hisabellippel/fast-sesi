44ebe86444e4cddc4037440231f5faad0e17e1c6
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-11-07 07:56:10 -0300

    tentativa de arrumar imagens de criar conta

--- public/paginaCriarConta.php
+++ public/paginaCriarConta.php
@@ -60,8 +60,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Funcionário</title>
+   <link rel="stylesheet" href="../style/style2.css">
     <link rel="stylesheet" href="../style/styles.css">
-    <link rel="stylesheet" href="../style/style2.css">
 
     <script>
         async function buscarCEP() {
