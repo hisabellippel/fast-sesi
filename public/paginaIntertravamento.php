cb2d2bf14cdf646959f1941debe6297327a5affd
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-11-07 08:15:30 -0300

    add - arrumei página intertravamento

--- public/paginaIntertravamento.php
+++ public/paginaIntertravamento.php
@@ -1,15 +1,12 @@
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Sinalização</title>
     <link rel="stylesheet" href="../style/styles.css">
-    <source src="login.js" type="">
 </head>
 
 <body>
-
     <header>
         <div id="barraescura">
             <a href="paginaSistemasdeSinalizacao.php"><img class="topo1" src="../asets/imagens/barraAcima/flecha.png" alt="">
@@ -22,35 +19,36 @@
         <div id="azul">
             <h2 id="hs">Sistema de sinalização:</h2>
         </div>
-        <br>
-        <br>
+       
 
-        <div class="redonda">
-            <a href="paginaSinaleiros.php"> <p class="cor" style="color: white;">Sinaleiros ▼ <span class="numero-vermelho" style="color: red;">°1</span></p></a>
-        </div>
-        
-        <div class="redonda">
-              
-            <a href="paginaPlacasSinalizar.php"> <p class="cor">Placas de Sinalização ▼ </p></a>
-        
-        </div>
+        <div class="pad3"></div>
+            <div class="redonda">
+                <a href="paginaSinaleiros.php"> <p class="cor" style="color: white;">Sinaleiros ▼ <span class="numero-vermelho" style="color: red;">°1</span></p></a>
+            </div>
             
-        
+            <div class="redonda">
+                <a href="paginaPlacasSinalizar.php"> <p class="cor">Placas de Sinalização ▼ </p></a>
+            </div>
+            
+            <div class="redonda">
+                <a href="paginaSistemadeSinalizacao2.php"> <p class="cor">CBTC (Communication-Based Train Control) ▼ <span class="numero-vermelho">°1</span></p></a>
+            </div>
 
-        <div class="redonda">
-            <a href="paginaSistemadeSinalizacao2.php"> <p class="cor">CBTC (Communication-Based Train Control) ▼ <span class="numero-vermelho">°1</span></p></a>
-        </div>
+            <div class="redonda">
+                <a href="paginaIntertravamento.php"> <p class="cor">Intertravamento ▼ </p></a>
+            
+            </div>
+            <div class="informacao">
+                <p>Nenhum problema relatado</p>
+            </div>
+        </div>    
+        <br>
+        <br>
+        <br>
+        <br>
+    </main>
 
-        <div class="redonda">
-            <a href="paginaIntertravamento.php"> <p class="cor">Intertravamento ▼ </p></a>
-        
-        </div>
-        <div class="informacao22222">
-            <p>nenhum problema relatado</p>
-        </div>
-        
-
-<br><br><br><br><br><br>
+    <footer>
         <div id="barra">
             <a href="paginainformacoes.php"><img class="topo1" src="../asets/imagens/barraAbaixo/barras.png" alt=""></a>
             <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
@@ -59,10 +57,7 @@
             <a href="paginaAlterarPerfil.php"><img class="im3" src="../asets/imagens/meio/perfil.png" alt=""></a>
             <a href="paginaPesquisar.php"><img class="im4" src="../asets/imagens/barraAbaixo/Lupa1.png" alt=""></a>
         </div>
-        <br>
-    </main>
-
+    </footer>
 
 </body>
-
 </html>
