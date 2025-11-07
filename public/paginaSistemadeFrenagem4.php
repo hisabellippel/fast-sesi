4019110bb984dce34445d6d16cea905d7e21918f
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-11-07 07:34:57 -0300

    add - css e arrumando páginas novas (sistema de frenagem)

--- public/paginaSistemadeFrenagem4.php
+++ public/paginaSistemadeFrenagem4.php
@@ -52,6 +52,7 @@
         </div>
 
     <br>
+    <br>
         <div id="barra">
             <a href="paginainformacoes.php"><img class="topo1" src="../asets/imagens/barraAbaixo/barras.png" alt=""></a>
             <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
