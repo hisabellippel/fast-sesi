526d902019114cd870281125cc71de1053cf9707
Julia Izabel <Julia_roza@estudante.sesisenai.org.br>
2025-11-11 09:59:26 -0300

    add arrumei gastos5

--- public/paginaGastos5.php
+++ public/paginaGastos5.php
@@ -1,11 +1,14 @@
-<html lang="en">
-
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gastos</title>
+    <title>Gastos5</title>
+
+    <link rel="stylesheet" href="../style/style2.css">
     <link rel="stylesheet" href="../style/styles.css">
-    <source src="login.js" type="">
+  
+    
 </head>
 
 <body>
@@ -16,56 +19,36 @@
             <img class="topo2" src="../asets/imagens/barraAcima/tradutor.png" alt="">
         </div>
     </header>
-    <br>
-    
+<br>
     <main>
-     
-
-        <div class="redonda3">
-            <p>Gastos <p class="red"></p>
+        <div class="redonda">
+            <p class="gastoss">Gastos</p>
         </div>
-        <div class="informacao23">
+
+        <div class="informacao" id="dados-gastos">
+            <h2 class="titulo">▼ Manutenções</h2>
+            <p data-func="Trns">Manutenções semanais dos trens: R$1.500,00</p>
+            <p data-func="Vias">Manutenções das vias: R$11.000,00</p>
+            <p data-func="Infraestrtura">Manutenções da infraestrutura auxiliar: R$10.000,00</p>
+            <p data-func="Vandalização">Manuntenção por vandalização: R$R$4.000,00</p>
+            
+        </div>
+<br>
+        <div class="redonda">
+            <p class="gastoss">Gráficos</p>
+        </div>
+
+        <div class="informacao">
             <br>
-            <div class="gastos2">
-                <br>
-                <p class="gastos"> ▼Manutenções</p>
-                <h6 class="gastos3">Manutenções semanais dos trens: R$1.500,00</h6>
-                <h6 class="gastos3">Manutenções das vias: R$11.000,00</h6>
-                <h6 class="gastos3">Manutenções da infraestrutura auxiliar: 10.000,00</h6>
-                <h6 class="gastos3">Manuntenção por vandalização: R$4.000,00</h6>
-                
-            
-                
-            </div>
-            
-
-          
-            
+            <canvas id="graficoGastos"></canvas>
         </div>
-
-        <div class="redonda3">
-            <p>Gráficos</p>
-        </div>
-        <div class="informacao2">
-            <br>
-            <img class= "grafico5" src="../asets/imagens/meio/grafico5.png" alt="">
-              
-        </div>
-
-      <br><br>
-
-        <div id="barra">
-            <a href="paginainformacoes.php"><img class="topo1" src="../asets/imagens/barraAbaixo/barras.png" alt=""></a>
-            <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
-            <h3>Fast.sesi</h3>
-            <a href="paginaNotificacoes.php"><img class="im2" src="../asets/imagens/barraAbaixo/sinoNotificacao.png" alt=""></a>
-            <a href="paginaAlterarPerfil.php"><img class="im3" src="../asets/imagens/meio/perfil.png" alt=""></a>
-            <a href="paginaPesquisar.php"><img class="im4" src="../asets/imagens/barraAbaixo/Lupa1.png" alt=""></a>
-        </div>
-       
     </main>
 
+    
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+      <script src="../scripts/grafico.js"></script>
+    
 
 </body>
-
 </html>
+
