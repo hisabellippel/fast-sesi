fa2ad4ec5c38663af030a96f432b9d8b28648479
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-11-11 07:36:21 -0300

    add- tentando ajeitar barra azul resistores

--- public/paginaResistoresdePotencia.php
+++ public/paginaResistoresdePotencia.php
@@ -66,7 +66,7 @@
     </main>
 
     <footer>
-        <div id="barra">
+        <div class="barraResistores">
             <a href="paginainformacoes.php"><img class="topo1" src="../asets/imagens/barraAbaixo/barras.png" alt=""></a>
             <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
             <h3>Fast.sesi</h3>
