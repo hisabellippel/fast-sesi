44d2e559e5f79ccfc50d1207163a0390cbf53c63
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-11-12 09:49:56 -0300

    add - arrumei alertas e notificações 2

--- public/paginaAlertaseNotificacoes3.php
+++ public/paginaAlertaseNotificacoes3.php
@@ -1,5 +1,4 @@
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +8,6 @@
 </head>
 
 <body>
-
     <header>
         <div id="barraescura">
             <a href="paginaAlertaseNotificacoes1.php"><img class="topo1" src="../asets/imagens/barraAcima/flecha.png" alt=""></a>
@@ -24,42 +22,35 @@
         </div>
 
 
-        <div class ="redondaequadrada">
-        <a href="paginaAlertaseNotificacoes2.php">
-           
-     <div class =  "letrabranca">
-        
-         <p class="maq">Alertas</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
-         </div>
-
-    </div>
-
-
-        
-        <div class ="redondaequadrada">
-
-            <div class =  "letrabranca">
-            <p class="maq">Notificações</p><p class="nova"><p class="nova2">▼</p>
+        <div class="pad6">
+            <div class ="redonda">
+                <a href="paginaAlertaseNotificacoes2.php">
+                    <div class =  "letrabranca">
+                        <p class="maq">Alertas</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
+                    </div>
+                </a>
             </div>
-   </div>
-    <div class = "informacao">
-    <p>Botão de emergencia acionado na linha 1</p>
 
-
-    </div>
-   </div>
+            <div class ="redonda">
+                <div class =  "letrabranca">
+                    <p class="maq">Notificações</p><p class="nova"><p class="nova2">▼</p>
+                </div>
+            </div>
+            <div class = "informacao5">
+                <p>Botão de emergencia acionado na linha 1</p>
+            </div>
+        </div>
     
           
     </main>
 
     <footer>
-        <div class="barra">
+        <div id="barra">
             <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
             <h3>Fast.sesi</h3>
             <a href="paginaAlterarPerfil.php"><img class="im3" src="../asets/imagens/meio/perfil.png" alt=""></a>
             <a href="paginaPesquisar.php"><img class="im4" src="../asets/imagens/barraAbaixo/Lupa1.png" alt=""></a>
         </div>
     </footer>
-
 </body>
 </html>
