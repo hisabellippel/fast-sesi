ae5c0e430184a6801250c4110ab816f1f0df7f26
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-11-12 08:46:24 -0300

    add - arrumei eixos ferroviários

--- public/paginaEixosFerroviarios3.php
+++ public/paginaEixosFerroviarios3.php
@@ -1,15 +1,13 @@
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trens Descarrilhados</title>
-    <link rel="stylesheet" href="../style/style2.css">
+    <title>Desgaste mecânico</title>
     <link rel="stylesheet" href="../style/styles.css">
+    <link rel="stylesheet" href="../style/style2.css">
 </head>
 
 <body>
-
     <header>
         <div id="barraescura">
             <a href="paginaEixosFerroviarios1.php"><img class="topo1" src="../asets/imagens/barraAcima/flecha.png" alt=""></a>
@@ -22,49 +20,44 @@
         <div id="azul">
             <h2 id="hs">Eixos ferroviários</h2>
         </div>
+        <br>
+        <br>
         
-
-        <div class ="redondaequadrada">
-        <a href="paginaEixosFerroviarios2.php">
-    <div class =  "letrabranca">
-        
-         <p class="maq">Trincas por fadiga</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
-         </div>
-
-    </div>
-
-
-
-        <div class ="redondaequadrada">
-
-            <div class =  "letrabranca">
-            <p class="maq">Desgaste mecânico</p><p class="nova"><p class="nova2">▼</p>
+        <div class="pad36">
+            <div class ="redonda">
+                <a href="paginaEixosFerroviarios2.php">
+                    <div class="letrabranca">
+                        <p class="maq">Trincas por fadiga</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
+                    </div>
+                </a>
             </div>
-   </div>
-   <div class = "informacao">
-    <p>Linha 5 necessita de troca de material devido ao contato excessivo com rolamentos</p>
 
-   </div>
+            <br>
 
-    
+            <div class ="redonda">
+                <div class="letrabranca">
+                    <p class="maq">Desgaste mecânico</p><p class="nova"><p class="nova2">▼</p>
+                </div>
+            </div>
+            <div class = "informacao5">
+                <p>Linha 5 necessita de troca de material devido ao contato excessivo com rolamentos</p>
+            </div>
 
-    <div class ="redondaequadrada">
-        <a href="paginaEixosFerroviarios4.php">
-    <div class =  "letrabranca">
-        
-         <p class="maq">Problemas de manutenção</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
-         </div>
+            <br>
 
-    </div>
-
-   
-             
-
-          
+            <div class ="redonda">
+                <a href="paginaEixosFerroviarios4.php">
+                    <div class="letrabranca">
+                        <p class="maq">Problemas de manutenção</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <br><br><br>
     </main>
 
     <footer>
-        <div class="Eixosbarra4">
+        <div id="barra">
             <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
             <h3>Fast.sesi</h3>
             <a href="paginaAlterarPerfil.php"><img class="im3" src="../asets/imagens/meio/perfil.png" alt=""></a>
--- public/paginaEixosFerroviarios4.php
+++ public/paginaEixosFerroviarios4.php
@@ -1,15 +1,13 @@
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trens Descarrilhados</title>
-    <link rel="stylesheet" href="../style/style2.css">
+    <title>Problemas de manutenção</title>
     <link rel="stylesheet" href="../style/styles.css">
+    <link rel="stylesheet" href="../style/style2.css">
 </head>
 
 <body>
-
     <header>
         <div id="barraescura">
             <a href="paginaEixosFerroviarios1.php"><img class="topo1" src="../asets/imagens/barraAcima/flecha.png" alt=""></a>
@@ -22,49 +20,49 @@
         <div id="azul">
             <h2 id="hs">Eixos ferroviários</h2>
         </div>
+        <br>
+        <br>
         
-       <div class ="redondaequadrada">
-        <a href="paginaEixosFerroviarios2.php">
-           
-     <div class =  "letrabranca">
-        
-         <p class="maq">Trincas por fadiga</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
-         </div>
-
-    </div>
-
-
-    <div class ="redondaequadrada">
-        <a href="paginaEixosFerroviarios3.php">
-    <div class =  "letrabranca">
-        
-         <p class="maq">Desgaste mecânico</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
-         </div>
-
-    </div>
-
-   <div class ="redondaequadrada">
-
-            <div class =  "letrabranca">
-            <p class="maq">Problemas de manutenção</p><p class="nova"><p class="nova2">▼</p>
+        <div class="pad36">
+            <div class ="redonda">
+                <a href="paginaEixosFerroviarios2.php">
+                    <div class="letrabranca">
+                        <p class="maq">Trincas por fadiga</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
+                    </div>
+                </a>
             </div>
-   </div>
-   <div class = "informacao">
-    <p>Linha 1 precisa de troca do eixo, foi encontrado uma deformação</p>
 
-   </div>
+            <br>
 
+            <div class ="redonda">
+                <a href="paginaEixosFerroviarios3.php">
+                    <div class="letrabranca">
+                        <p class="maq">Desgaste mecânico</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
+                    </div>
+                </a>
+            </div>
 
+            <br>
+
+            <div class ="redonda">
+                <div class="letrabranca">
+                    <p class="maq">Problemas de manutenção</p><p class="nova"><p class="nova2">▼</p>
+                </div>
+            </div>
+            <div class = "informacao5">
+                <p>Linha 1 precisa de troca do eixo, foi encontrado uma deformação</p>
+            </div>
+        </div>    
+        <br><br><br>
    </main>
 
     <footer>
-        <div class="Eixosbarra5">
+        <div id="barra">
             <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
             <h3>Fast.sesi</h3>
             <a href="paginaAlterarPerfil.php"><img class="im3" src="../asets/imagens/meio/perfil.png" alt=""></a>
             <a href="paginaPesquisar.php"><img class="im4" src="../asets/imagens/barraAbaixo/Lupa1.png" alt=""></a>
         </div>
     </footer>
-
 </body>
 </html>
