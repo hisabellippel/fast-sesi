e219c75f756d3dc2666817fc171e224deb0b40a6
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-11-12 09:53:54 -0300

    add arrumei trilhos3

--- public/paginaTrilhos3.php
+++ public/paginaTrilhos3.php
@@ -49,11 +49,13 @@
     </main>
 
     <footer>
+         <div class= "pads1">
         <div id="barra">
             <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
             <h3>Fast.sesi</h3>
             <a href="paginaAlterarPerfil.php"><img class="im3" src="../asets/imagens/meio/perfil.png" alt=""></a>
             <a href="paginaPesquisar.php"><img class="im4" src="../asets/imagens/barraAbaixo/Lupa1.png" alt=""></a>
+        </div>
         </div>
     </footer>
 
