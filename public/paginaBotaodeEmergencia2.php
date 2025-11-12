d3d3c7b5a8cd371f7f0ec09eba2f022cd2f1c7cf
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-11-12 08:20:31 -0300

    add - arrumei botão de emergência 1, 2 e 3

--- public/paginaBotaodeEmergencia2.php
+++ public/paginaBotaodeEmergencia2.php
@@ -1,15 +1,13 @@
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trens Descarrilhados</title>
-    <link rel="stylesheet" href="../style/style2.css">
     <link rel="stylesheet" href="../style/styles.css">
+    <link rel="stylesheet" href="../style/style2.css">
 </head>
 
 <body>
-
     <header>
         <div id="barraescura">
             <a href="paginaBotaodeEmergencia1.php"><img class="topo1" src="../asets/imagens/barraAcima/flecha.png" alt=""></a>
@@ -22,42 +20,33 @@
         <div id="azul">
             <h2 id="hs">Botão de emergência</h2>
         </div>
+        <br>
+        <br>
         
-        <div class ="redondaequadrada">
-
-            <div class =  "letrabranca">
-            <p class="maq">Maquinista</p><p class="nova"><p class="nova2">▼</p>
+        <div class="pad7">
+            <div class ="redonda">
+                <div class="letrabranca">
+                    <p class="maq">Maquinista</p><p class="nova"><p class="nova2">▼</p>
+                </div>
             </div>
-   </div>
-   <div class = "informacao">
-    <p>Botão acionado na linha dois</p>
+            <div class="informacao5">
+                <p>Botão acionado na linha dois</p>
+            </div>
 
-  </div>
+            <br>
+            <br>
 
-
-   <div class ="redondaequadrada">
-        <a href="paginaBotaodeEmergencia3.php">
-           
-     <div class =  "letrabranca">
-        
-         <p class="maq">Bagageiro</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
-         </div>
-
-    </div>
-
-
- 
-
-    
-     
-        
-       
-
-          
-    </main>
-
+            <div class ="redonda">
+                <a href="paginaBotaodeEmergencia3.php">
+                    <div class="letrabranca">
+                        <p class="maq">Bagageiro</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
+                    </div>
+                </a>    
+            </div>
+        </div>
+</main>
     <footer>
-        <div class="barra">
+        <div id="barra">
             <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
             <h3>Fast.sesi</h3>
             <a href="paginaAlterarPerfil.php"><img class="im3" src="../asets/imagens/meio/perfil.png" alt=""></a>
--- public/paginaBotaodeEmergencia3.php
+++ public/paginaBotaodeEmergencia3.php
@@ -1,15 +1,13 @@
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trens Descarrilhados</title>
-    <link rel="stylesheet" href="../style/style2.css">
     <link rel="stylesheet" href="../style/styles.css">
+    <link rel="stylesheet" href="../style/style2.css">
 </head>
 
 <body>
-
     <header>
         <div id="barraescura">
             <a href="paginaBotaodeEmergencia1.php"><img class="topo1" src="../asets/imagens/barraAcima/flecha.png" alt=""></a>
@@ -22,46 +20,36 @@
         <div id="azul">
             <h2 id="hs">Botão de emergência</h2>
         </div>
+        <br>
+        <br>
         
-       
-
-
-   <div class ="redondaequadrada">
-        <a href="paginaBotaodeEmergencia2.php">
-           
-     <div class =  "letrabranca">
-        
-         <p class="maq">Maquinista</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
-         </div>
-
-    </div>
-
-
-
-    <div class ="redondaequadrada">
-
-            <div class =  "letrabranca">
-            <p class="maq">Bagageiro</p><p class="nova"><p class="nova2">▼</p>
+        <div class="pad7">
+            <div class ="redonda">
+                <a href="paginaBotaodeEmergencia2.php">
+                    <div class="letrabranca">
+                        <p class="maq">Maquinista</p><p class="nova"><strong>°1</strong></p><p class="nova2">▼</p>
+                    </div>
+                </a>
             </div>
-   </div>
-   <div class = "informacao">
-    <p>Nenhum acionamento relatado.</p>
 
-  </div>
+            <br>
+            <br>
+            <br>
 
-
- 
-
-    
-     
-        
-       
-
-          
+            <div class ="redonda">
+                <div class="letrabranca">
+                    <p class="maq">Bagageiro</p><p class="nova"><p class="nova2">▼</p>
+                </div>
+            </div>
+            <div class = "informacao5">
+                <p>Nenhum acionamento relatado.</p>
+            </div>
+        </div>    
+        <br>
     </main>
 
     <footer>
-        <div class="barra">
+        <div id="barra">
             <img class="logo" src="../asets/imagens/barraAbaixo/logo.png" alt="">
             <h3>Fast.sesi</h3>
             <a href="paginaAlterarPerfil.php"><img class="im3" src="../asets/imagens/meio/perfil.png" alt=""></a>
