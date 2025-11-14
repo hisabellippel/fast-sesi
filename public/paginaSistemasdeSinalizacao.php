2621375034efa6939a3429648a3dc1e838a70a53
Ana <ana.l.amaral@estudante.sesisenai.org.br>
2025-11-14 10:11:31 -0300

    Voltando a página anterior

--- public/paginaSistemasdeSinalizacao.php
+++ public/paginaSistemasdeSinalizacao.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Sinalização</title>
     <link rel="stylesheet" href="../style/styles.css">
-    <link rel="stylesheet" href="../style/style3.css">
+    <link rel="stylesheet" href="../style/styles2.css">
 </head>
 
 <body>
@@ -20,7 +20,33 @@
         <div id="azul">
             <h2 id="hs">Sistema de sinalização:</h2>
         </div>
+        <br>
+        <br>
+        <div class="pad18">
+            <div class="redonda">
+                <a href="paginaSinaleiros.php">
+            <p class="cor">Sinaleiros  <span class="numero-vermelho">°1</p></a> <p class="cor">▼</p>
+            </div>
 
+            <br>
+
+            <div class="redonda">
+                <a href="paginaPlacasSinalizar.php"> <p class="cor">Placas de sinalização▼</p> </a>
+            </div>
+
+            <br>
+
+            <div class="redonda">
+                <a href="paginaCBTC.php"> <p class="cor"><p class="cor">CBTC (Communication-Based Train Control)<span class="numero-vermelho">°1</p></a> </p></a><p class="cor">▼</p>
+            </div>
+
+            <br>
+
+            <div class="redonda">
+                <a href="paginaIntertravamento.php"><p class="cor">Intertravamento▼</p></a>
+            </div>
+        </div>
+        <br> <br>
 
         <div class="fundo">
 
@@ -50,25 +76,25 @@
             </form>
 
             <div class="lado">
-                <div class="red000">
-                    <img src="../asets/imagens/meio/sinaleiro.png" alt="" height= "60px" width= "60px">
-                    <a href="paginaSinaleiros.php"><p class="cormenu">Sinaleiros</p></a>
+                <div class="red00">
+                    <img src="../asets/imagens/meio/botaoemergencia.png" alt="" height= "60px" width= "60px">
+                    <a href="paginaBotaodeEmergencia1.php"><p class="cormenu">Botão de Emergência</p></a>
                 </div>
 
-                <div class="red1111">
-                    <img src="../asets/imagens/meio/placa.png" alt="" height= "70px" width= "70px">
-                    <a href="paginaPlacasSinalizar.php"><p class="cormenu">Placas de Sinalização</p></a>
+                <div class="red111">
+                    <img src="../asets/imagens/meio/eixos.png" alt="" height= "70px" width= "70px">
+                    <a href="paginaEixosFerroviarios1.php"><p class="cormenu">Eixos Ferroviários</p></a>
                 </div>
             </div>
             <div class="lado">
-                <div class="red2222">
-                    <img src="../asets/imagens/meio/cbtc.png" alt="" height= "50px" width= "60px">
-                    <a href="paginaCBTC.php"><p class="cormenu">CBTC</p></a>
+                <div class="red222">
+                    <img src="../asets/imagens/meio/frenagem.png" alt="" height= "50px" width= "60px">
+                    <a href="paginaSistemadeFrenagem1.php"><p class="cormenu">Sistema de Frenagem</p></a>
                 </div>
 
-                <div class="red3333">
-                    <img src="../asets/imagens/meio/intertravamento.png" alt="" height= "60px" width= "70px">
-                    <a href="paginaIntertravamento.php"><p class="cormenu">Intertravamento</p></a>
+                <div class="red333">
+                    <img src="../asets/imagens/meio/potencia.png" alt="" height= "60px" width= "70px">
+                    <a href="paginaResistoresdePotencia.php"><p class="cormenu">Resitores de potência</p></a>
                 </div>
             </div>
         </div>
