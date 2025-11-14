ccd4c7b54c1bef1dec3bc0a95742ced93c269ed1
Ana <ana.l.amaral@estudante.sesisenai.org.br>
2025-11-14 10:17:02 -0300

    Delete - pages

--- public/paginaSistemasdeSinalizacao.php
+++ public/paginaSistemasdeSinalizacao.php
@@ -48,56 +48,7 @@
         </div>
         <br> <br>
 
-        <div class="fundo">
-
-        <br>
-            
-         <form action="pesquisa.php" method="get" class="form-pesquisa" autocomplete="off">
-            <input
-                type="text"
-                id="c-pesquisa"
-                name="q"
-                placeholder="Digite (ex: linhas, trens, relatórios...)"
-                list="sugestoes"
-                required
-            >
-            <datalist id="sugestoes">
-                <option value="linhas">
-                <option value="trens">
-                <option value="inspecao">
-                <option value="relatorio">
-                <option value="analises">
-                <option value="ouvidoria">
-                <option value="alertas">
-                <option value="notificacoes">
-                <option value="gastos">
-            </datalist>
-            <button type="submit">Buscar</button>
-            </form>
-
-            <div class="lado">
-                <div class="red00">
-                    <img src="../asets/imagens/meio/botaoemergencia.png" alt="" height= "60px" width= "60px">
-                    <a href="paginaBotaodeEmergencia1.php"><p class="cormenu">Botão de Emergência</p></a>
-                </div>
-
-                <div class="red111">
-                    <img src="../asets/imagens/meio/eixos.png" alt="" height= "70px" width= "70px">
-                    <a href="paginaEixosFerroviarios1.php"><p class="cormenu">Eixos Ferroviários</p></a>
-                </div>
-            </div>
-            <div class="lado">
-                <div class="red222">
-                    <img src="../asets/imagens/meio/frenagem.png" alt="" height= "50px" width= "60px">
-                    <a href="paginaSistemadeFrenagem1.php"><p class="cormenu">Sistema de Frenagem</p></a>
-                </div>
-
-                <div class="red333">
-                    <img src="../asets/imagens/meio/potencia.png" alt="" height= "60px" width= "70px">
-                    <a href="paginaResistoresdePotencia.php"><p class="cormenu">Resitores de potência</p></a>
-                </div>
-            </div>
-        </div>
+        
     </main>
 
     <footer>
