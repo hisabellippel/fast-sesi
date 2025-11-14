1ff7428b30d0b74dbc8f0d962a5f9dbced711f9b
Ana <ana.l.amaral@estudante.sesisenai.org.br>
2025-11-14 10:06:39 -0300

    Arrumando a página sistema de sinalização

--- public/paginaSistemasdeSinalizacao.php
+++ public/paginaSistemasdeSinalizacao.php
@@ -51,23 +51,23 @@
 
             <div class="lado">
                 <div class="red000">
-                    <img src="../asets/imagens/meio/botaoemergencia.png" alt="" height= "60px" width= "60px">
+                    <img src="../asets/imagens/meio/sinaleiro.png" alt="" height= "60px" width= "60px">
                     <a href="paginaSinaleiros.php"><p class="cormenu">Sinaleiros</p></a>
                 </div>
 
                 <div class="red1111">
-                    <img src="../asets/imagens/meio/eixos.png" alt="" height= "70px" width= "70px">
+                    <img src="../asets/imagens/meio/placa.png" alt="" height= "70px" width= "70px">
                     <a href="paginaPlacasSinalizar.php"><p class="cormenu">Placas de Sinalização</p></a>
                 </div>
             </div>
             <div class="lado">
                 <div class="red2222">
-                    <img src="../asets/imagens/meio/frenagem.png" alt="" height= "50px" width= "60px">
+                    <img src="../asets/imagens/meio/cbtc.png" alt="" height= "50px" width= "60px">
                     <a href="paginaCBTC.php"><p class="cormenu">CBTC</p></a>
                 </div>
 
                 <div class="red3333">
-                    <img src="../asets/imagens/meio/potencia.png" alt="" height= "60px" width= "70px">
+                    <img src="../asets/imagens/meio/intertravamento.png" alt="" height= "60px" width= "70px">
                     <a href="paginaIntertravamento.php"><p class="cormenu">Intertravamento</p></a>
                 </div>
             </div>
