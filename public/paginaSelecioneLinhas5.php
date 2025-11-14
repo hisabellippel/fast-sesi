16db57f6b47c5d21e1b85a629dc89968b4f2cd75
Ana <ana.l.amaral@estudante.sesisenai.org.br>
2025-11-14 09:16:40 -0300

    Add - informação as linhas

--- public/paginaSelecioneLinhas5.php
+++ public/paginaSelecioneLinhas5.php
@@ -49,13 +49,13 @@
             </div>
 
             <div class="velocidade">
-                <p> Velocidade</p>
+                <p> Velocidade: </p>
             </div>
             <div class="passageiros">
-                <p>Passageiros em tempo real</p>
+                <p>Passageiros em tempo real: <p class="numero-vermelho"> 974 </p></p>
             </div>
             <div class="alertaseno">
-                <p>Alertas e Notificações</p>
+                <p>Alertas e Notificações: <p class="numero-vermelho"> Nenhum </p></p>
             </div>
 
 
