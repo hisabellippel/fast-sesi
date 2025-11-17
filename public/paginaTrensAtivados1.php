60ef762d4268e069e5bd43881a7c8a95a8a4c5f7
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-11-17 09:08:46 -0300

    add arrumei trilhos para o novo padrao

--- public/paginaTrensAtivados1.php
+++ public/paginaTrensAtivados1.php
@@ -21,10 +21,9 @@
 </style>
 </head>
 <body>
- 
   <header>
    <div id="barraescura">
-            <a href="paginaRelatorioeAnalises.php"><img class="topo1" src="../asets/imagens/barraAcima/flecha.png" alt="">
+            <a href="paginaMenuPrincipal.php"><img class="topo1" src="../asets/imagens/barraAcima/flecha.png" alt="">
             <a href="paginaNotificacoes.php"><img class="im2" src="../asets/imagens/barraAbaixo/sinoNotificacao.png" alt=""></a>
         </div>
   </header>
