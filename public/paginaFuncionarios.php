c067c7d2e0fe3cbbf81891f5685847ff06f6267f
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-11-27 09:28:45 -0300

    add- erro corrigido

--- public/paginaFuncionarios.php
+++ public/paginaFuncionarios.php
@@ -146,7 +146,7 @@ if (isset($_GET['delete']) && isset($_GET['credencial_funcionario'])) {
                     $cpf = $row['cpf_funcionario'];
                     $senha_mascarada = str_repeat('*', 8);
                     $cpf_formatted = substr($cpf, 0, 3) . '.' . substr($cpf, 3, 3) . '.' . substr($cpf, 6, 3) . '-' . substr($cpf, 9, 2);
-                    $img_path = "public/uploads/uploads/" . htmlspecialchars($row['foto_funcionario']);
+                    $img_path = "uploads/uploads/" . htmlspecialchars($row['foto_funcionario']);
                     $img_src = ($row['foto_funcionario'] && $row['foto_funcionario'] != 'default.jpg' && file_exists($img_path)) ? "uploads/uploads/" . htmlspecialchars($row['foto_funcionario']) : "../asets/imagens/meio/rostoAlterarPerfil.png";
 
 $bg_color = 'white';
